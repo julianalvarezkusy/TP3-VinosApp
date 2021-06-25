dd93f1453212ec6853be2c659c038618d1fb1be9
julianalvarezkusy <60948729+julianalvarezkusy@users.noreply.github.com>
2021-06-24 21:30:12 -0300

    Correcciones

--- app/src/main/java/com/example/vinos_app/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/vinos_app/fragments/DetailsFragment.kt
@@ -5,32 +5,20 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
-<<<<<<< HEAD
-<<<<<<< HEAD
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-=======
->>>>>>> 4dcf3670023c8d16888333765fb6a82ee4f40183
-=======
->>>>>>> 4dcf3670023c8d16888333765fb6a82ee4f40183
 import android.widget.TextView
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.example.vinos_app.R
-<<<<<<< HEAD
-<<<<<<< HEAD
 import com.example.vinos_app.viewModel.CreateUserViewModel
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.*
 import java.lang.Exception
-=======
-import com.google.android.material.snackbar.Snackbar
->>>>>>> 4dcf3670023c8d16888333765fb6a82ee4f40183
-=======
-import com.google.android.material.snackbar.Snackbar
->>>>>>> 4dcf3670023c8d16888333765fb6a82ee4f40183
+
+
+
 
 class DetailsFragment : Fragment() {
 
@@ -89,65 +77,54 @@ class DetailsFragment : Fragment() {
         wineName.text = "Nombre: " + wineObj.nombre
         wineCellar.text = "Cellar: " + wineObj.bodega
         wineRating.text = "Rating: " + wineObj.rating
+
+
+        wineFavourite.setOnClickListener()
+        {
+
+            val parentJob = Job()
+            val scope = CoroutineScope(Dispatchers.Default + parentJob)
+
+            scope.launch {
+                try {
+                    val userConected = userViewModel.getUserConected()
+                    if (userConected != null) {
+                        val getUserbyEmail = async { userConected.email?.let { it1 -> userViewModel.getUserByEmail(it1) } }
+                        val user = getUserbyEmail.await()
+                        if (user != null) {
+                            viewModel.addWine(user, wineObj)
+                            Snackbar.make(v, "Vino agregado correctamente", Snackbar.LENGTH_SHORT)
+                                    .show()
+                        } else {
+                            Log.d("Error", "No se encontró el usuario")
+                        }
+                    }
+
+
+                } catch (e: Exception) {
+                    Log.d("", "ERROR: " + e.message)
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.details_wine_toolbar, menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-        wineFavourite.setOnClickListener {
-
-            val parentJob = Job()
-            val scope = CoroutineScope(Dispatchers.Default + parentJob)
-
-            scope.launch {
-                try{
-                    val userConected = userViewModel.getUserConected()
-                    if(userConected!= null){
-                        val getUserbyEmail = async { userConected.email?.let { it1 -> userViewModel.getUserByEmail(it1) } }
-                        val user = getUserbyEmail.await()
-                        if(user != null){
-                            viewModel.addWine(user , wineObj)
-                            Snackbar.make(v, "Vino agregado correctamente", Snackbar.LENGTH_SHORT)
-                                    .show()
-                        }else{
-                            Log.d("Error", "No se encontró el usuario")
-                        }
-                    }
-
-
-                }catch (e : Exception){
-                    Log.d("","ERROR: "+ e.message)
-                }
 
 
 
-=======
->>>>>>> 4dcf3670023c8d16888333765fb6a82ee4f40183
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-<<<<<<< HEAD
-            }
 
 
 
 
-        }
-
-=======
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
->>>>>>> 4dcf3670023c8d16888333765fb6a82ee4f40183
 
         val id = when(item.itemId) {
 
-=======
-        val id = when(item.itemId) {
-
->>>>>>> 4dcf3670023c8d16888333765fb6a82ee4f40183
             R.id.app_bar_switch -> Snackbar.make(v, "bar_switch", Snackbar.LENGTH_SHORT).show()
 
             else -> ""
